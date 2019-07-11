db14b2da8e06ac6a43b92f91f30b807e1817ca70
aghost <ggg17226@gmail.com>
2019-07-11 18:03:15 +0800

    add ccm ram dynamic control library

---
+++ aghostCcmramCtrl/aghostCcmramCtrl.c
@@ -0,0 +1,167 @@
+//
+// Created by aghost on 2019/7/10.
+//
+
+#include <sched.h>
+#include "aghostCcmramCtrl.h"
+
+CCMRAM uint8_t aghostCcmram[AGHOST_CCMRAM_CTRL_SIZE];
+CCMRAM void *lastByte;
+CCMRAM  aghostMemBlock *firstBlock;
+
+
+void mergeFreeSpace() {
+    //first block
+    aghostMemBlock *ptr = (aghostMemBlock *) &aghostCcmram[0];
+    while (1) {
+        // finish merge condition
+        if (ptr->next == NULL) {
+            return;
+        }
+        //find and merge free space
+        aghostMemBlock *nextPtr = ptr->next;
+        if (ptr->attr & AGHOST_MEM_BLOCK_ATTR_FREE == AGHOST_MEM_BLOCK_ATTR_FREE &&
+            nextPtr->attr & AGHOST_MEM_BLOCK_ATTR_FREE == AGHOST_MEM_BLOCK_ATTR_FREE) {
+            ptr->next = nextPtr->next;
+            nextPtr->before = NULL;
+            ptr->size += nextPtr->size + AGHOST_MEM_BLOCK_SIZE;
+        } else {
+            ptr = nextPtr;
+            continue;
+        }
+    }
+}
+
+void aghostCcmramInit(void) {
+    void *pos = &aghostCcmram[0];
+    uint16_t count = AGHOST_CCMRAM_CTRL_SIZE;
+    while (count--) {
+        *(char *) pos = 0;
+        pos = (char *) pos + 1;
+    }
+    lastByte = &aghostCcmram[AGHOST_CCMRAM_CTRL_SIZE];
+    firstBlock = (aghostMemBlock *) &aghostCcmram[0];
+    firstBlock->before = NULL;
+    firstBlock->next = NULL;
+    firstBlock->attr = AGHOST_MEM_BLOCK_ATTR_FREE;
+    firstBlock->size = sizeof(aghostCcmram) - AGHOST_MEM_BLOCK_SIZE;
+}
+
+
+void *aghostCcmramMalloc(size_t size) {
+    // check size
+    if (size < AGHOST_CCMRAM_CTRL_MIN_ALLOC_SIZE) {
+        size = AGHOST_CCMRAM_CTRL_MIN_ALLOC_SIZE;
+    } else {
+        if (size % AGHOST_CCMRAM_CTRL_ALLOC_ALIGN_NUM != 0) {
+            size = (size >> AGHOST_CCMRAM_CTRL_ALLOC_MOVEBIT + 1)
+                    << AGHOST_CCMRAM_CTRL_ALLOC_MOVEBIT;
+        }
+    }
+
+    //first block
+    aghostMemBlock *block = firstBlock;
+    if (
+            (block->attr & AGHOST_MEM_BLOCK_ATTR_FREE == AGHOST_MEM_BLOCK_ATTR_FREE) &&
+            (block->size >= (size + AGHOST_MEM_BLOCK_SIZE))
+            ) {
+
+        aghostMemBlock *newBlock = (aghostMemBlock *) ((void *) block + AGHOST_MEM_BLOCK_SIZE + size);
+        newBlock->before = block;
+        newBlock->next = NULL;
+        newBlock->size = block->size - AGHOST_MEM_BLOCK_SIZE - size;
+        newBlock->attr = AGHOST_MEM_BLOCK_ATTR_FREE;
+        block->attr = AGHOST_MEM_BLOCK_ATTR_USED;
+        block->size = size;
+        block->next = newBlock;
+        return ((void *) block) + AGHOST_MEM_BLOCK_SIZE;
+    }
+        //find free block
+    else {
+        while (1) {
+            if (block->next == NULL) {
+                return NULL;
+            }
+            block = block->next;
+            if (
+                    (block->attr & AGHOST_MEM_BLOCK_ATTR_FREE == AGHOST_MEM_BLOCK_ATTR_FREE) &&
+                    (block->size >= (size + AGHOST_MEM_BLOCK_SIZE))
+                    ) {
+                aghostMemBlock *newBlock = (aghostMemBlock *) ((void *) block + AGHOST_MEM_BLOCK_SIZE + size);
+                newBlock->before = block;
+                newBlock->next = NULL;
+                newBlock->size = block->size - AGHOST_MEM_BLOCK_SIZE - size;
+                newBlock->attr = AGHOST_MEM_BLOCK_ATTR_FREE;
+                block->attr = AGHOST_MEM_BLOCK_ATTR_USED;
+                block->size = size;
+                block->next = newBlock;
+                return ((void *) block) + AGHOST_MEM_BLOCK_SIZE;
+            } else {
+                continue;
+            }
+        }
+    }
+}
+
+void *aghostCcmramCalloc(size_t size) {
+    // check size
+    if (size < AGHOST_CCMRAM_CTRL_MIN_ALLOC_SIZE) {
+        size = AGHOST_CCMRAM_CTRL_MIN_ALLOC_SIZE;
+    } else {
+        if (size % AGHOST_CCMRAM_CTRL_ALLOC_ALIGN_NUM != 0) {
+            size = (size >> AGHOST_CCMRAM_CTRL_ALLOC_MOVEBIT + 1)
+                    << AGHOST_CCMRAM_CTRL_ALLOC_MOVEBIT;
+        }
+    }
+    //alloc space
+    void *ptr = aghostCcmramMalloc(size);
+    if (ptr == NULL)return NULL;
+    else {
+        // init space
+        void *ptrFirstByte = ptr;
+        while (size--) {
+            *(char *) ptr = 0;
+            ptr = (char *) ptr + 1;
+        }
+        return ptrFirstByte;
+    }
+}
+
+
+void aghostCcmramFree(void *ptr) {
+    if (ptr <= (void *) &aghostCcmram[0] || ptr >= lastByte) {
+        return;
+    }
+    aghostMemBlock *block = (aghostMemBlock *) (ptr - AGHOST_MEM_BLOCK_SIZE);
+    aghostMemBlock *firstPtr = firstBlock;
+    //free first block
+    if (block == firstPtr) {
+        if ((block->attr << 7 >> 7) == AGHOST_MEM_BLOCK_ATTR_USED) {
+            block->attr = AGHOST_MEM_BLOCK_ATTR_FREE;
+            if (AGHOST_CCMRAM_CTRL_USE_FREE_SPACE_MERGE) mergeFreeSpace();
+        }
+        return;
+    }
+    //check block
+    if (
+            (block->attr << 7 >> 7) != AGHOST_MEM_BLOCK_ATTR_USED ||
+            block->before == NULL
+            ) {
+        return;
+    }
+    //search first block
+    aghostMemBlock *searchBlockPtr = firstPtr;
+    while (searchBlockPtr->next != NULL) {
+        searchBlockPtr = searchBlockPtr->next;
+        if (searchBlockPtr == block) {
+            block->attr = AGHOST_MEM_BLOCK_ATTR_FREE;
+            if (AGHOST_CCMRAM_CTRL_USE_FREE_SPACE_MERGE) mergeFreeSpace();
+            return;
+        }
+    }
+}
+
+
+
+
+
---
+++ aghostCcmramCtrl/aghostCcmramCtrl.h
@@ -0,0 +1,134 @@
+/*
+ *  Created by aghost on 2019/7/10.
+ *  last edit on 2019/7/10
+ *
+ *  dynamic control for stm32 ccmram
+ *
+ *  please edit these macro to fit your program and hardware
+ *    AGHOST_CCMRAM_CTRL_SIZE
+ *    AGHOST_CCMRAM_CTRL_MIN_ALLOC_SIZE
+ *    AGHOST_CCMRAM_CTRL_ALLOC_ALIGN - 2/4/8/16/32
+ *    CCMRAM - this macro must be the same as ccmram section name in ld script
+ *    AGHOST_CCMRAM_CTRL_USE_SHORT_FUNC_NAME
+ */
+
+#ifndef AGHOST_CCMRAM_CTRL_H
+#define AGHOST_CCMRAM_CTRL_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+
+#define AGHOST_CCMRAM_CTRL_USE_SHORT_FUNC_NAME
+
+#ifndef AGHOST_CCMRAM_CTRL_VERSION
+#define AGHOST_CCMRAM_CTRL_VERSION 1
+#endif
+
+#ifdef AGHOST_CCMRAM_CTRL_USE_SHORT_FUNC_NAME
+#define ccmInit() aghostCcmramInit()
+#define ccmMalloc(size) aghostCcmramMalloc(size)
+#define ccmCalloc(size) aghostCcmramCalloc(size)
+#define ccmFree(ptr) aghostCcmramFree(ptr)
+#define ccmramMalloc(size) aghostCcmramMalloc(size)
+#define ccmramCalloc(size) aghostCcmramCalloc(size)
+#define ccmramFree(ptr) aghostCcmramFree(ptr)
+#endif
+
+#ifndef AGHOST_CCMRAM_CTRL_USE_FREE_SPACE_MERGE
+#define AGHOST_CCMRAM_CTRL_USE_FREE_SPACE_MERGE 1
+#endif
+
+#ifndef AGHOST_CCMRAM_CTRL_SIZE
+/**
+ * number of bytes
+ * 49152 = 48k
+ * 61952 = 60.5k
+ */
+#define AGHOST_CCMRAM_CTRL_SIZE 49152
+#endif
+
+#ifndef AGHOST_CCMRAM_CTRL_MIN_ALLOC_SIZE
+#define AGHOST_CCMRAM_CTRL_MIN_ALLOC_SIZE 4
+#endif
+
+#ifndef AGHOST_CCMRAM_CTRL_ALLOC_ALIGN
+#define AGHOST_CCMRAM_CTRL_ALLOC_ALIGN 4
+#endif
+
+#if AGHOST_CCMRAM_CTRL_ALLOC_ALIGN == 2
+#define AGHOST_CCMRAM_CTRL_ALLOC_MOVEBIT 1
+#define AGHOST_CCMRAM_CTRL_ALLOC_ALIGN_NUM 2
+
+#elif AGHOST_CCMRAM_CTRL_ALLOC_ALIGN == 4
+#define AGHOST_CCMRAM_CTRL_ALLOC_MOVEBIT 2
+#define AGHOST_CCMRAM_CTRL_ALLOC_ALIGN_NUM 4
+
+#elif AGHOST_CCMRAM_CTRL_ALLOC_ALIGN == 8
+#define AGHOST_CCMRAM_CTRL_ALLOC_MOVEBIT 3
+#define AGHOST_CCMRAM_CTRL_ALLOC_ALIGN_NUM 8
+
+#elif AGHOST_CCMRAM_CTRL_ALLOC_ALIGN == 16
+#define AGHOST_CCMRAM_CTRL_ALLOC_MOVEBIT 4
+#define AGHOST_CCMRAM_CTRL_ALLOC_ALIGN_NUM 16
+
+#elif AGHOST_CCMRAM_CTRL_ALLOC_ALIGN == 32
+#define AGHOST_CCMRAM_CTRL_ALLOC_MOVEBIT 5
+#define AGHOST_CCMRAM_CTRL_ALLOC_ALIGN_NUM 32
+
+#else
+#define AGHOST_CCMRAM_CTRL_ALLOC_MOVEBIT 2
+#define AGHOST_CCMRAM_CTRL_ALLOC_ALIGN_NUM 4
+
+#endif
+
+
+#ifndef CCMRAM
+#define CCMRAM __attribute__((section(".ccmram")))
+#endif
+
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
+
+#ifndef AGHOST_MEM_BLOCK
+#define AGHOST_MEM_BLOCK
+typedef struct aghostMemBlock_t {
+    struct aghostMemBlock_t *before; //4byte
+    struct aghostMemBlock_t *next;   //4byte
+    uint8_t attr;                    //1byte
+    uint16_t size;                   //2byte
+} aghostMemBlock;
+#endif
+
+#ifndef AGHOST_MEM_BLOCK_ATTRS
+#define AGHOST_MEM_BLOCK_ATTRS
+
+#define AGHOST_MEM_BLOCK_ATTR_FREE 0b00000001
+#define AGHOST_MEM_BLOCK_ATTR_USED 0b00000000
+
+#endif
+
+
+#ifndef AGHOST_MEM_BLOCK_SIZE
+#define AGHOST_MEM_BLOCK_SIZE sizeof(struct aghostMemBlock_t)
+#endif
+
+
+void aghostCcmramInit(void);
+
+void *aghostCcmramMalloc(size_t size);
+
+void *aghostCcmramCalloc(size_t size);
+
+void aghostCcmramFree(void *ptr);
+
+#ifdef __cplusplus
+}
+#endif
+#endif //AGHOSTCCMRAMCTRL_H
